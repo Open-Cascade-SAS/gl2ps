ede48cd8aaef99be721129a7baab3a6ef7bd30d0
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2003-10-28 06:23:36 +0000

    Fix typos

--- gl2ps.c
+++ gl2ps.c
@@ -1,4 +1,4 @@
-/* $Id: gl2ps.c,v 1.131 2003-10-28 05:41:50 geuzaine Exp $ */
+/* $Id: gl2ps.c,v 1.132 2003-10-28 06:23:36 geuzaine Exp $ */
 /*
  * GL2PS, an OpenGL to PostScript Printing Library
  * Copyright (C) 1999-2003 Christophe Geuzaine <geuz@geuz.org>
@@ -7,17 +7,17 @@
  * modify it under the terms of either:
  *
  * a) the GNU Library General Public License as published by the Free
- * Software Foundation; either version 2 of the Licnese, or (at your
+ * Software Foundation; either version 2 of the License, or (at your
  * option) any later version, or
  *
  * b) the GL2PS License as published by Christophe Geuzaine, either
- * version 1, or (at your option) any later version.
+ * version 1 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either
- * the GNU Libary General Public License or the GL2PS License for more
- * details.
+ * the GNU Library General Public License or the GL2PS License for
+ * more details.
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library in the file named "COPYING.LGPL";
@@ -25,8 +25,8 @@
  * Cambridge, MA 02139, USA.
  *
  * You should also have received a copy of the GL2PS License with this
- * libary in the file named "COPYING.GL2PS"; if not, I will be glad to
- * provide one.
+ * library in the file named "COPYING.GL2PS"; if not, I will be glad
+ * to provide one.
  *
  * Contributors:
  *   Michael Sweet <mike@easysw.com>
